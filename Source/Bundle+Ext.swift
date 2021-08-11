1ad32970949dd17b8b45185343ab8464273b1177
Umar Farooq <dev.omar561@gmail.com>
2021-08-12 00:38:44 +0500

    Center image crash fixed

---
+++ Source/Bundle+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle+Ext.swift
+//  CBSpinner
+//
+//  Created by Umar Farooq on 11/08/2021.
+//
+
+import Foundation
---
+++ Source/UIImage+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Ext.swift
+//  CBSpinner
+//
+//  Created by Umar Farooq on 12/08/2021.
+//
+
+import Foundation
